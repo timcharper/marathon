adcf5d854fa23461666c2b83d32d2908738adf5d
Tim Harper <tharper@mesosphere.com>
2017-05-04 22:01:20 -0600

    WIP: AppDefinition update support
    needs tests; other API methods not implemented

--- src/main/scala/mesosphere/marathon/api/akkahttp/Directives.scala
+++ src/main/scala/mesosphere/marathon/api/akkahttp/Directives.scala
@@ -1,10 +1,30 @@
 package mesosphere.marathon
 package api.akkahttp
-import akka.http.scaladsl.server.{ Directives => AkkaDirectives }
+import akka.http.scaladsl.server.{ Directive1, Directives => AkkaDirectives }
+import com.wix.accord.{ Success, Failure, Validator }
 
 /**
   * All Marathon Directives and Akka Directives
   *
   * These should be imported by the respective controllers
   */
-object Directives extends AuthDirectives with LeaderDirectives with AkkaDirectives
+object Directives extends AuthDirectives with LeaderDirectives with AkkaDirectives {
+  val RemainingPathId = {
+    import mesosphere.marathon.state.PathId.StringPathId
+    RemainingPath.map(_.toString.toRootPath)
+  }
+
+  /**
+    * Validate the given resource using the implicit validator in scope; reject if invalid
+    *
+    * Ideally, we validate while unmarshalling; however, in the case of app updates, we need to apply validation after
+    * applying some update operation.
+    */
+  def validated[T](resource: T)(implicit validator: Validator[T]): Directive1[T] = {
+    validator(resource) match {
+      case Success => provide(resource)
+      case failure: Failure =>
+        reject(EntityMarshallers.ValidationFailed(failure))
+    }
+  }
+}
